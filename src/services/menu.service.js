afe37ec6fc67ea8b2ad56f04abda9fe5149e3c9a
Patcharapong K <6230362521@student.chula.ac.th>
2023-09-13 06:47:37 +0700

    GPT improved GetMenu, GetMenus functions

--- src/services/menu.service.js
+++ src/services/menu.service.js
@@ -1,9 +1,42 @@
-function GetMenus(call, callback) {
-  // logic to fetch menus from database and send back to client
+const grpc = require("@grpc/grpc-js");
+const Menu = require("../models/menu.model");
+
+async function GetMenus(call, callback) {
+  try {
+    const menus = await Menu.find();
+    callback(null, { menus: menus });
+  } catch (err) {
+    callback({
+      code: grpc.status.INTERNAL,
+      details: "Failed to fetch menus",
+    });
+  }
 }
 
-function GetMenu(call, callback) {
-  // logic to fetch a single menu based on given ID and send back to client
+async function GetMenu(call, callback) {
+  const menuId = call.request.id;
+  if (!menuId) {
+    return callback({
+      code: grpc.status.INVALID_ARGUMENT,
+      details: "Menu ID is required",
+    });
+  }
+
+  try {
+    const menu = await Menu.findById(menuId);
+    if (!menu) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        details: "Menu not found",
+      });
+    }
+    callback(null, { menu: menu });
+  } catch (err) {
+    callback({
+      code: grpc.status.INTERNAL,
+      details: "Failed to fetch the menu",
+    });
+  }
 }
 
 // ... other RPC method implementations
