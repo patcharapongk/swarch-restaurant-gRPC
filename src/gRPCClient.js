d5feda0f5f524a458ce90b2eb15c5fb95dfcfe98
Patcharapong K <6230362521@student.chula.ac.th>
2023-09-13 15:00:12 +0700

    Fix path __dirname problem

--- src/gRPCClient.js
+++ src/gRPCClient.js
@@ -1,8 +1,9 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "../config.env" });
+const path = require("path");
+dotenv.config({ override: true, path: path.join(__dirname, "../config.env") });
 
 /* gRPC Client Headers */
-const PROTO_PATH = "../restaurant_proto.proto";
+const PROTO_PATH = path.join(__dirname, "..", "proto", "restaurant.proto");
 
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
@@ -15,9 +16,19 @@ const pkgDefinition = protoLoader.loadSync(PROTO_PATH, {
 });
 const protoDescriptor = grpc.loadPackageDefinition(pkgDefinition);
 
+const port = process.env.GRPC_SERVER_PORT;
+console.log(`client port = ${port}`);
+
 const client = new protoDescriptor.RestaurantService(
-  `${process.env.GRPC_CLIENT_URL}:${process.env.GRPC_CLIENT_PORT}`,
+  `${process.env.GRPC_CLIENT_URL}:${port}`,
   grpc.credentials.createInsecure()
+);
+console.log(
+  "\n----------------\ngRPC Client running at " +
+    process.env.GRPC_CLIENT_URL +
+    ":" +
+    port +
+    ""
 );
 
 module.exports = client;
